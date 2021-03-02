51e967afa81b37d327fd93107a54434379409894
Graeme Porteous <graeme@rgbp.co.uk>
2021-03-02 09:25:07 +0000

    Remove example failing spec

--- spec/icasework_spec.rb
+++ spec/icasework_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Icasework do
   it 'has a version number' do
     expect(Icasework::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
