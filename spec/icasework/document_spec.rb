0f6b8278769a0ee6f476d19143b69174c4fe1eae
Graeme Porteous <graeme@rgbp.co.uk>
2021-03-19 09:11:49 +0000

    Extract document contents of PDF files

--- spec/icasework/document_spec.rb
+++ spec/icasework/document_spec.rb
@@ -47,4 +47,47 @@ RSpec.describe Icasework::Document do
       it { expect(document.attributes[:id]).to eq 'D225851' }
     end
   end
+
+  describe '#pdf?' do
+    subject { instance.pdf? }
+
+    let(:instance) { described_class.new(type: type) }
+
+    context 'with PDF mimitype' do
+      let(:type) { 'application/pdf' }
+
+      it { is_expected.to eq true }
+    end
+
+    context 'with other mimitype' do
+      let(:type) { 'plain/text' }
+
+      it { is_expected.to eq false }
+    end
+  end
+
+  describe '#pdf_contents' do
+    subject { instance.pdf_contents }
+
+    let(:instance) { described_class.new(type: type, __content__: url) }
+    let(:url) { 'https://example.com/document.pdf' }
+
+    before do
+      stub_request(:get, url).to_return(
+        status: 200, body: File.new('spec/fixtures/document.pdf')
+      )
+    end
+
+    context 'with PDF mimitype' do
+      let(:type) { 'application/pdf' }
+
+      it { is_expected.to eq 'Hello World' }
+    end
+
+    context 'with othe mimitype' do
+      let(:type) { 'plain/text' }
+
+      it { is_expected.to eq nil }
+    end
+  end
 end
